460cfbeaf3b9e592e6567596c5d65c065d95891a
Anjali <anjalijanar@gmail.com>
2023-02-17 10:49:12 +0530

    Basic of Object

---
+++ ObjectDemo.js
@@ -0,0 +1,12 @@
+let studClassObj = {
+    "Anjali":"BCA",
+    "Tannu":"Five",
+    "Appu":"BSC"
+};
+console.log(studClassObj);
+// let obj1 = {"Neha":"BED"};
+// studClassObj.push()
+studClassObj["Neha"] = "BED";
+studClassObj["Anjali"] = "MCA";
+console.log(studClassObj);
+console.log(studClassObj["Goltu"]);
