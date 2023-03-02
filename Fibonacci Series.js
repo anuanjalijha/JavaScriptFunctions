06b6c2bf1fbd62cdfad2f438de9e656c7eca958f
Anjali <anjalijanar@gmail.com>
2023-03-02 10:54:36 +0530

    Create an array and store the square of first 50 natural numbers

---
+++ Fibonacci Series.js
@@ -0,0 +1,10 @@
+let n=5;
+let first_term = 0;
+let second_term = 1;
+let next_term;
+for(let i=0;i<n;i++){
+    console.log(first_term);
+    next_term=first_term+second_term;
+    first_term = second_term;
+    second_term = next_term;
+}
